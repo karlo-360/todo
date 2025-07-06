97438fc6234e682e32ba703e918c504261cfe4b0
karlo-360 <karlod1130@gmail.com>
2025-07-05 19:32:50 -0700

    add comand list

--- cmd/list.go
+++ cmd/list.go
@@ -1,11 +1,14 @@
 /*
 Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
+	"encoding/csv"
 	"fmt"
+	"log"
+	"os"
+	"text/tabwriter"
 
 	"github.com/spf13/cobra"
 )
@@ -13,15 +16,36 @@ import (
 // listCmd represents the list command
 var listCmd = &cobra.Command{
 	Use:   "list",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "list all tasks",
+	Long: `list all the tasks save for the user`,
+	Args: cobra.NoArgs,
+	DisableFlagsInUseLine: true,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("list called")
+		fr, err := os.Open("/home/karlo/Documents/tasks.csv")
+		if err != nil {
+			log.Fatalln("error opening the file for reading: ", err)
+		}
+
+		r := csv.NewReader(fr)
+
+		tasks, err := r.ReadAll()
+		if err != nil {
+			log.Fatalln("error reading all: ", err)
+		}
+
+		if err := fr.Close(); err != nil{
+			log.Fatalln("error closing file reader: ", err)
+		}
+
+		w := tabwriter.NewWriter(os.Stdout, 0, 2, 4, ' ', 0,)
+		for _, v := range tasks{
+			fmt.Fprintf(w, "%s\t%s\t%s\n", v[0], v[1], v[2])
+		}
+
+		if err := w.Flush(); err != nil{
+			log.Fatalln("error while flushing: ", err)
+		}
+
 	},
 }
 
